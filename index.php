1ddffd6d71134d1671ab6b7d02c1ec762ae9b26e
internetperson <mike_novellino4biz@aol.com>
2018-03-18 04:54:14 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,12 @@
 <body>
   <!–– Use how you want! Just be sure to credit me! ––>
   <h1>TextPaste: A simple way to create and share .txt files. Coded by internetperson.</h1>
-
 <form method="post" action="">
-       <TEXTAREA name="typed" rows="20" cols="80" value="<?= isset($_POST['paste']) ? htmlspecialchars($_POST['paste']) : '' ?>" >
+       <TEXTAREA name="typed" rows="20" cols="80" value="<?= isset($_POST['typed']) ? htmlspecialchars($_POST['typed']) : '' ?>" >
    
    </TEXTAREA> <!–– thanks to @nekofurball for fixing this for me! ––>
     
-<input type="submit" value="Paste!" />
+    <input type="submit" value="Paste!" />
   </form>
 
 <?php
@@ -19,12 +18,11 @@ $seed = str_split('abcdefghijklmnopqrstuvwxyz'
 shuffle($seed);
 $rand = '';
 foreach (array_rand($seed, 5) as $k) $rand .= $seed[$k];
-if(isset($_POST['paste'])) {
+if(isset($_POST['typed'])) {
   $my_file = $rand.'.txt';
   $handle = fopen($my_file, 'w') or die('Cannot open file: '.$my_file);
-  $txt = htmlspecialchars($_POST['paste']);
+  $txt = htmlspecialchars($_POST['typed']);
   fwrite($handle, $txt);
   echo 'https://ip1000.ml/textpaste/'.$my_file.' - Latest file you created.';
 }
 ?>
-
